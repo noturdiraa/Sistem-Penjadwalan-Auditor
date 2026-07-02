22bee9da7d9eac464992e6bf75c8ed2715e40cb5
kiaaacantik <kiaazkia274@gmail.com>
2026-07-02 14:40:52 +0700

    Memperbarui sidebar layout

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,50 @@
+<div class="bg-dark text-white p-3"
+     style="width:250px; min-height:100vh;">
+
+    <h5 class="text-center mb-4">
+
+        MENU
+
+    </h5>
+
+    <a href="#" class="d-block text-white text-decoration-none mb-3">
+
+        <i class="fas fa-home"></i>
+
+        Dashboard
+
+    </a>
+
+    <a href="#" class="d-block text-white text-decoration-none mb-3">
+
+        <i class="fas fa-users"></i>
+
+        Data Auditor
+
+    </a>
+
+    <a href="#" class="d-block text-white text-decoration-none mb-3">
+
+        <i class="fas fa-award"></i>
+
+        Kompetensi
+
+    </a>
+
+    <a href="#" class="d-block text-white text-decoration-none mb-3">
+
+        <i class="fas fa-user"></i>
+
+        Profil
+
+    </a>
+
+    <a href="#" class="d-block text-danger text-decoration-none mt-5">
+
+        <i class="fas fa-sign-out-alt"></i>
+
+        Logout
+
+    </a>
+
+</div>
