af0c9748be8e73ae52146ad0fc9434c8962760c7
noturdiraa <fadillahrahmaputririzal_23021450027@radenfatah.ac.id>
2026-08-12 08:21:17 +0700

    Security: Revert DatabaseSeeder changes to prevent hardcoding default user credentials in codebase

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,54 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $defaultUsers = [
-            [
-                'nama_user' => 'Administrator',
-                'username' => 'adminbspji',
-                'password' => Hash::make('BSPJI123'),
-                'password_plain' => 'BSPJI123',
-                'role' => 'Admin',
-            ],
-            [
-                'nama_user' => 'kepegawaianbspji',
-                'username' => 'kepegawaianbspji',
-                'password' => Hash::make('bspji123'),
-                'password_plain' => 'bspji123',
-                'role' => 'Kepegawaian',
-            ],
-            [
-                'nama_user' => 'pjibspji',
-                'username' => 'pjibspji',
-                'password' => Hash::make('bspji123'),
-                'password_plain' => 'bspji123',
-                'role' => 'PJI',
-            ],
-            [
-                'nama_user' => 'operasionalbspji',
-                'username' => 'operasionalbspji',
-                'password' => Hash::make('bspji123'),
-                'password_plain' => 'bspji123',
-                'role' => 'Operasional',
-            ],
-            [
-                'nama_user' => 'kepalabalaibspji',
-                'username' => 'kepalabalaibspji',
-                'password' => Hash::make('bspji123'),
-                'password_plain' => 'bspji123',
-                'role' => 'Kepala Balai',
-            ],
-        ];
-
-        foreach ($defaultUsers as $u) {
-            User::firstOrCreate(
-                ['username' => $u['username']],
-                [
-                    'nama_user' => $u['nama_user'],
-                    'password' => $u['password'],
-                    'password_plain' => $u['password_plain'],
-                    'role' => $u['role'],
-                ]
-            );
-        }
+        //
     }
 }
